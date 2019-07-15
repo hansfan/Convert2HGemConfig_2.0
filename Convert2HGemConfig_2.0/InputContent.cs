f5cb0fe071a6e1f19fe23b43babea4b71b81181e
Hans <tuxedovan@gmail.com>
2019-07-15 15:06:36 +0800

    first

---
+++ Convert2HGemConfig_2.0/InputContent.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputStream
+{
+    class InputContent
+    {
+        private string _path;
+        private CheckInputStream checkInputStream = new CheckInputStream();
+        public InputContent(string Path)
+        {
+            this._path = Path;
+        }
+
+        private bool checkEmtpyCategory(string lineString)
+        {
+            string[] array = lineString.Split(',');
+
+            string value = array[0];
+
+            if (value.Trim() == string.Empty)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public List<string> getInputContent()
+        {
+            List<string> col = new List<string>();
+          
+            string tmp = string.Empty;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(_path))
+                {
+                    while (reader.Peek() >= 0)
+                    {
+                        tmp = reader.ReadLine().Trim();
+
+                        if (checkEmtpyCategory(tmp))
+                        {
+                            continue;
+                        }
+
+                        if (!checkInputStream.CheckHGemConfig(tmp))
+                        {
+                            tmp = ";;;;;;;;" + tmp;
+                        }
+                        col.Add(tmp);
+
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return col;
+
+        }
+
+    }
+
+
+    class CheckInputStream
+    {
+        private List<string> ecList = new List<string>();
+        private List<string> vidList = new List<string>();
+        private List<string> eventList = new List<string>();
+        private List<string> reportList = new List<string>();
+        private List<string> linkList = new List<string>();
+
+        public CheckInputStream()
+        {
+
+        }
+
+
+        public bool CheckHGemConfig(string lineString)
+        {
+            string[] arraySplit;
+            string category = string.Empty;
+            string key = string.Empty;
+            bool result = false;
+
+            try
+            {
+                arraySplit = lineString.Split(',');
+                category = arraySplit[0].Trim().ToLower();
+                key = arraySplit[1].Trim();
+
+                if (category == "ec")
+                {
+                    if (!ecList.Contains(key))
+                    {
+                        ecList.Add(key);
+                        result = true;
+                    }
+                }
+                else if (category == "vid")
+                {
+                    if (!vidList.Contains(key))
+                    {
+                        vidList.Add(key);
+                        result = true;
+                    }
+                }
+                else if (category == "event")
+                {
+                    if (!eventList.Contains(key))
+                    {
+                        vidList.Add(key);
+                        result = true;
+                    }
+                }
+                else if (category == "report")
+                {
+                    if (!reportList.Contains(key))
+                    {
+                        reportList.Add(key);
+                        result = true;
+                    }
+                }
+                else if (category == "reportlink")
+                {
+                    if (!linkList.Contains(key))
+                    {
+                        linkList.Add(key);
+                        result = true;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
+            return result;
+        }
+
+
+
+
+    }
+}
---
+++ Convert2HGemConfig_2.0/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Convert2HGemConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please,key in InputFullPath and OutputFullPath.");
+            }
+            else
+            {
+                string inputPath = args[0].Trim().ToString();
+
+                MainProcess(inputPath);
+
+
+            }
+
+            Console.WriteLine("Success!");
+            Console.Read();
+
+        }
+
+        static public void MainProcess(string input)
+        {
+            InputStream.InputContent inputStream = new InputStream.InputContent(input);
+            Convertor convertor = new Convertor();
+            List<List<string>> HGemconfigList = convertor.Process(inputStream.getInputContent());
+
+            CheckHGemConfig check = new CheckHGemConfig();
+            String result = check.Check(HGemconfigList);
+
+            OutputStream outputStream = new OutputStream();
+            string output = outputStream.outputFullPath(input);
+            outputStream.toHGemConfig(output, result);
+
+
+        }
+
+
+
+
+    }
+
+    class OutputStream
+    {
+        public OutputStream()
+        {
+
+        }
+
+        public void toHGemConfig(string fullPath, string obj)
+        {
+            try
+            {
+                using (StreamWriter output = new StreamWriter(fullPath))
+                {
+                    output.WriteLine(obj);
+                }
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+
+        public string outputFullPath(string input)
+        {
+            string result = string.Empty;
+            string fileName = string.Empty;
+            int pPosition = -1;
+            try
+            {
+                string[] arr = input.Split('\\');
+                fileName = arr[arr.Length - 1];
+                pPosition = fileName.LastIndexOf('.');
+                fileName = fileName.Substring(0, pPosition) + ".cfg";
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    result += arr[i]+"\\";
+                }
+                result += fileName;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return result;
+        }
+
+
+    }
+
+
+}
